e132d08fb76a0cfccc8a3310a6f22195e7a3165c
GollyTicker <golly.ticker@gmail.com>
2014-04-05 21:13:51 +0200

    before writing [(Int,Int)] -> [Int]

--- fst/src/A3_Poly.scala
+++ fst/src/A3_Poly.scala
@@ -5,11 +5,21 @@ object A3_Poly extends App {
 
 
   def test(a: AnyRef, b: AnyRef) = {
-    val tpl = if (a == b) (" ==","") else ("/=", "\nExpected: " + a + "\nResult:" + b
-    "a " + tpl._1 + " b" + tpl._2
+    val tpl = if (a == b)
+      (" == ", "")
+    else (" /= ", "\nExpected: " + a + "\nResult:" + b)
+    "a" + tpl._1 + "b" + tpl._2
+  }
+}
+
+class Polynom private (csAssoc: List[Pair[Int,Int]] ) {
+
+  val cs = fromAssocList(csAssoc)
+
+  def fromAssocList(as:List[Pair[Int,Int]]):List[Int] = {
+    Nil
   }
 
-class Polynom private (val cs: List[Int] ) {
   def apply (x: Int) = cs.foldLeft (0) ((a, c) => a * x + c)
 
   def + (p: Polynom) = p
@@ -20,7 +30,10 @@ class Polynom private (val cs: List[Int] ) {
 
 
 object Polynom {
-  def apply (cFirst: Int, cs: Int *) = new Polynom (cFirst :: cs.toList)
-  def apply (cFirst: Pair[Int, Int], cs: Pair[Int, Int] ) = new Polynom (0 :: Nil) //TODO: allow tuplesd input
+  def apply (cFirst: Int, csRest: Int *) = {
+    val cs = cFirst :: csRest.toList
+    new Polynom(cs.reverse.zipWithIndex)
+  }
+  def apply (cFirst: Pair[Int, Int], cs: Pair[Int, Int]* ) = new Polynom (cFirst :: cs.toList)
 
 }
