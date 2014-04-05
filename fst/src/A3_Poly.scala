c7e390e7905e80a5f44d83d152ca15dac243977d
GollyTicker <golly.ticker@gmail.com>
2014-04-05 21:50:43 +0200

    blubb...

--- fst/src/A3_Poly.scala
+++ fst/src/A3_Poly.scala
@@ -1,9 +1,7 @@
 /**
  * Created by Swaneet on 05.04.2014.
  */
-object A3_Poly{
-
-
+object PolyA3{
   def test(a: AnyRef, b: AnyRef) = {
     val tpl = if (a == b)
       (" == ", "")
@@ -24,23 +22,29 @@ object A3_Poly{
 class Polynom private (csAssoc: List[Pair[Int,Int]] ) {
 
   val cs = fromAssocList(csAssoc)
+  val len = cs.length
 
   def fromAssocList(as:List[Pair[Int,Int]]):Vector[Int] = {
+          // (exponent, coefficient)
     println("AssocList: " + as)
 
-    val maxC:Int = as.maxBy(_._1)._1
-    val poly:Array[Int] = new Array(maxC) // the new keyword prevents a single element Array
-    as.foreach( (tpl) => tpl match { case (exp, c) => require(exp >= 0); poly(exp) = c} )
-    val c=poly.toVector
+    val maxC:Int = as.maxBy(_._1)._1 + 1 // the plus 1 is because the zero counts as a coefficient
+    val poly:Array[Int] = new Array[Int](maxC) // the new keyword prevents a single element Array
+    println("intArray: " + poly.deep)
+    as.foreach( (tpl) => tpl match { case (exp, c) => require(exp >= 0); println("got: " + tpl);poly(exp) = c} )
+    val c=poly.toVector.reverse
     println("Vector: " + c)
     c
   }
 
-  def apply (x: Int) = cs.foldLeft (0) ((a, c) => a * x + c)
+
+  def apply (x: Int) = cs.foldRight(0) ((a, c) => a * x + c)
 
   def + (p: Polynom) = p
   def * (p: Polynom) = p
   def ° (p: Polynom) = p
+
+  override def toString = cs.toString //(cs.zipWithIndex.foldLeft ( ("", 0) ) ( (strExp, c) => (c + "x^" + strExp._2 + " " + strExp._1, strExp._2 + 1))) ._1
 
 }
 
@@ -48,7 +52,8 @@ class Polynom private (csAssoc: List[Pair[Int,Int]] ) {
 object Polynom {
   def apply (cFirst: Int, csRest: Int *) = {
     val cs = cFirst :: csRest.toList
-    new Polynom(cs.reverse.zipWithIndex)
+    val withExponents = cs.reverse.zipWithIndex.map(_.swap)
+    new Polynom(withExponents)
   }
   def apply (cFirst: Pair[Int, Int], cs: Pair[Int, Int]* ) = new Polynom (cFirst :: cs.toList)
 
