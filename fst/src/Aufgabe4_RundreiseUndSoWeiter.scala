3a3a3f09fa6adadd65305ed5e7405e3e73a7d324
GollyTicker <golly.ticker@gmail.com>
2014-04-13 17:03:46 +0200

    A1 fin

--- fst/src/Aufgabe4_RundreiseUndSoWeiter.scala
+++ fst/src/Aufgabe4_RundreiseUndSoWeiter.scala
@@ -5,21 +5,42 @@ import scala.compat.Platform
  */
 object A {
 
-  def distance(c1: Int, c2: Int):Double = {
-    if (c1 == c2) return 0
+  def run() {
+    var failed = false
+    // helper function to display test results
+    def test(a: Any, b: Any) = {
+      val tpl = if (a == b) {
+        (" == ", "")
+      }
+      else {
+        failed = true
+        (" /= ", "\nExpected: " + a + "\nResult:" + b)
+      }
+      println(a.toString() + tpl._1.toString() + b.toString() + tpl._2.toString())
+    }
+
+    // test works, disabled due to 4sec calculation time.
+    // test(shortestRoute(List(1,2,3,4)), (133, List(2,4,1,3)) )
+  }
+
+  def distance(tpl:(Int,Int)):Int = {
+    val c1 = tpl._1
+    val c2 = tpl._2
+
     val diff = if (c1>c2) c1 - c2 else c2 - c1
     100 / diff
   }
 
-  def shortestRoute(ls:List[Int]):(Double, List[Int]) = {
-
-    val init = (Double.PositiveInfinity, Nil)
-
-    def step (tpl:(Double, List[Int])) (x:Int):(Double, List[Int]) = tpl match {
-      case (currMin, currMinList) => (currMin, currMinList)
+  def shortestRoute(ls:List[Int]):(Int, List[Int]) = {
+    require( ! ls.isEmpty )
+    def calcDist(ps:List[Int]):(Int, List[Int]) = {
+      val dist = ps.zip(ps.tail).map( distance _ ).sum
+      (dist, ps)
     }
-
-    ls.permutations.foldLeft(init)( step(_)(_) )
+    ls
+      .permutations     // all Tours
+      .map( calcDist )  // assign a cost to each tour
+      .minBy( _._1 )    // take the first with minimal cost.
   }
 
 }
