6fb6a9e98ceb92e113350f4e69fbe4b226283457
GollyTicker <golly.ticker@gmail.com>
2014-04-18 03:23:07 +0200

    base finished

--- fst/src/A5.scala
+++ fst/src/A5.scala
@@ -1,30 +1,70 @@
 /**
  * Created by Swaneet on 18.04.2014.
  */
+
+object Run {
+  def run() = {
+    println(Action.make("<-", "MC") over MC.inital)  // should be 1 1 ~~~ 2 2
+  }
+}
+
 object MC {
-  lazy val inital = State(0,0,3,3)
+  lazy val inital = State.make(0,0,MS,CS)
+
+  val MS = 3
+  val CS= 3
+  val NPPL = CS + MS
 
   def isGameOver(st:State) = st match {
     case State(ml,cl,mr,cr) => ml < cl || mr < cr
   }
 
-  def __action_over(act:Action)(st:State) = throw new RuntimeException
+  def isGameFinished(st:State) = st match {
+    case State(`MS`,`CS`,0,0) => true   // *almost PROLOG style matching here*
+    case _ => false
+  }
+
+  def __action_over(act:Action)(st:State):Option[State] = {
+    None
+  }
 
 }
 
 // missionary left, cannibal left, massionary right, cannibal right
-case class State(val ml:Int, val cl:Int, val mr:Int, val cr:Int) {
-  override def toString = ml + " " cl + " ~~~ " + mr + " " + cr
+case class State private (val ml:Int, val cl:Int, val mr:Int, val cr:Int) {
+  override def toString = ml + " " + cl + " ~~~ " + mr + " " + cr
 }
+object State {
+  def make(ml:Int, cl:Int, mr:Int, cr:Int) = {
+    val ls = List(ml,cl,mr,cr)
+    assert(ls.sum == MC.NPPL)
+    assert(ls.forall( _ >= 0))
+    State(ml,cl,mr,cr)
+  }
+}
+
 // from right to left, passengers
-case class Action(val rtoleft:Boolean, val psg:Boarding) {
+case class Action private (val rtoleft:Boolean, val psg:Boarding) {
   override def toString = if (rtoleft)
                               "<-" + psg + "<-"
                               else "->" + psg + "->"
-  def over(st:State) = MC.__action_over(this)(st)
+
+  def over(st:State):Option[State] = MC.__action_over(this)(st)
 }
 
-class Boarding private (val str:String)
+object Action {
+  def make(dirStr:String, psgStr:String) = {
+    val rtoleft = if (dirStr=="<-") true
+                  else if (dirStr=="->") false
+                  else throw new RuntimeException
+    val psg = Boarding(psgStr)
+    Action(rtoleft, psg)
+  }
+}
+
+class Boarding private (val str:String) {
+  override def toString = str
+}
 
 object Boarding {
   def apply(psg:String) = new Boarding (psg match {
