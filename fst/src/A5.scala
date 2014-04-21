0f1ec12180bd838bf0f47dc2e560a800657f2a5b
GollyTicker <golly.ticker@gmail.com>
2014-04-21 18:35:58 +0200

    going solver now

--- fst/src/A5.scala
+++ fst/src/A5.scala
@@ -4,25 +4,30 @@
 
 object Run {
   def run() = {
-    println(MC.initial takes Action.make("<-", "MC"))
-    val fst = MC.chain("<-","CC")(MC.initial lift)
-    println(fst)
 
     val solution:List[Action] = List("<-","->","<-","->","<-","->","<-","->","<-","->","<-")
                           .zip(List("MC", "M","CC", "C","MM","MC","MM", "C","CC", "C","CC"))
-                          .map( tpl => Action.make(tpl._1, tpl._2) )
+                          .map( tpl => Action(tpl._1, tpl._2) )
 
-    println(MC.play(MC.begin)(solution))
+    println("Beginning: " + MC.startGame(List()))
+    println("Sample Move: " + MC.play("<-", "MC")(MC.beginning))
+    println("Ending: " + MC.startGame(solution))
   }
 }
 
 object MC {
   lazy val initial = State.make(0,0,MS,CS, false)
-  lazy val begin = initial lift
+  lazy val beginning = initial lift
 
   val MS = 3
   val CS= 3
   val NPPL = CS + MS
+
+  type GameProgress = Option[(State,String)]
+    // None => invalid Turn
+    // Some(st. "W") => won
+    // Some(st, "C") => to be continued
+    // Some(st, "L") => game over (lost)
 
   def debug(str:String):Unit = println(str)
 
@@ -35,34 +40,33 @@ object MC {
     case _ => false
   }
 
-  def play(game:GameProgress)(ls:List[Action]):GameProgress = ls.foldLeft(game)( (yet,act) => chainAct(act)(yet) )
-
-  def chainAct(act:Action)(prev:GameProgress):GameProgress = {
+  // thows away the progress, because one may not do any action now,
+  // unless the game is still to be continued ("C")
+  // if the game is to be continued, then the action is executed
+  def play_(act:Action)(prev:GameProgress):GameProgress = {
     prev match {
-      case None => {debug("chain1"); None}
-      case Some((_, "W")) => {debug("chain1"); None}
-      case Some((_, "L")) => {debug("chain1"); None}
-      case Some((st, "C")) => st takes act
+      case None => {debug("play_1: cannot process because invalid Progress"); None}
+      case Some((_, "W")) => {debug("play_2: Game is already won."); None}
+      case Some((_, "L")) => {debug("play_3: Game is already over."); None}
+      case Some((st, "C")) => st takes act  // here the action is executed
       case Some((_, _)) => throw new RuntimeException
     }
   }
+  // play is the user friendly variant. play_ is the actual implementation
+  def play(dirs:String, psgs:String) = play_(Action(dirs,psgs)) _
 
-  def chain(dirStr:String, psgStr:String)(prev:GameProgress):GameProgress = {
-    chainAct(Action.make(dirStr, psgStr))(prev)
-  }
+  def startGame = playFrom(beginning) _
 
-  type GameProgress = Option[(State,String)]
-        // None => invalid Turn
-        // Some(st. "W") => won
-        // Some(st, "C") => to be continued
-        // Some(st, "L") => game over (lost)
+  // playes the actiosn from a given Progress.
+  def playFrom(game:GameProgress)(ls:List[Action]):GameProgress = ls.foldLeft(game)( (yet,act) => play_(act)(yet) )
 
+  // the actual implementation of game logic.
   def _action_over_state(act:Action)(st:State):GameProgress = {
     val lch:Int => Int = x => x + (if (act.rtoleft) (1) else (-1))
     val rch:Int => Int = x => x + (if (act.rtoleft) (-1) else (1))
 
     if (act.rtoleft == !st.isLeft) ( () ) // all ok!
-              else return {debug("boat wrong"); None}  // bad. wrong direction
+              else return {debug("_action_over_state: boat wrong"); None}  // bad. wrong direction
 
     // the ch stands for "change". mlch means the change in numbers of missionaries on the left side
     var mlch:Int => Int = x => x
@@ -89,21 +93,23 @@ object MC {
         State.safeMake(newML, newCL, newMR, newCR, newIsLeft)
       }
     }
-    checkEnding(newOptionState)
+    handleIfEndOfGame(newOptionState)
   }
 
-  def checkEnding(newOptSt:Option[State]):GameProgress =
+  def handleIfEndOfGame(newOptSt:Option[State]):GameProgress =
     for {
         newSt <- newOptSt
-        result = () match { // because if else if else look so ugly here
+        result = () match { // using match expression because "if else if else" looks ugly.
           case _ if MC.isGameOver(newSt) => (newSt, "L")
           case _ if MC.isGameFinished(newSt) => (newSt, "W")
           case _ => (newSt, "C")
         }
     } yield result
 
+  // for a given game progress, "solve" returns a list of actions for which this game
+  // can be finished properly. If this isnt possible, then it returns None.
+  def solve(yet:GameProgress):Option[List[Action]] = None
 }
-
 // missionary left, cannibal left, massionary right, cannibal right
 case class State private (val ml:Int, val cl:Int, val mr:Int, val cr:Int, val isLeft:Boolean) {
   override def toString = {
@@ -111,10 +117,11 @@ case class State private (val ml:Int, val cl:Int, val mr:Int, val cr:Int, val is
     ml + " " + cl + "  " + riverAndBoat + " " + mr + " " + cr
   }
   def takes(act:Action):MC.GameProgress = MC._action_over_state(act)(this)
-  lazy val lift:MC.GameProgress = MC.checkEnding(Some(this))
+  lazy val lift:MC.GameProgress = MC.handleIfEndOfGame(Some(this))
 }
 
 object State {
+  // not using apply here, because I want a return type of Option[State] (see safeMake) insteadt of State.
   def make(ml:Int, cl:Int, mr:Int, cr:Int, isLeft:Boolean) = {
     safeMake(ml,cl,mr,cr, isLeft).get
   }
@@ -128,19 +135,19 @@ object State {
 }
 
 // from right to left, passengers
-case class Action private (val rtoleft:Boolean, val psg:Boarding) {
+class Action private (val rtoleft:Boolean, val psg:Boarding) {
   override def toString = if (rtoleft)
                               "<-" + psg + "<-"
                               else "->" + psg + "->"
 }
 
 object Action {
-  def make(dirStr:String, psgStr:String) = {
+  def apply(dirStr:String, psgStr:String) = {
     val rtoleft = if (dirStr=="<-") true
                   else if (dirStr=="->") false
                   else throw new RuntimeException
     val psg = Boarding(psgStr)
-    Action(rtoleft, psg)
+    new Action(rtoleft, psg)
   }
 }
 
