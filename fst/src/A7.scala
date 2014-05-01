c697a65c4350b624e9458d79befa83ef61c47f86
GollyTicker <golly.ticker@gmail.com>
2014-05-01 13:47:44 +0200

    A7 finished.

--- fst/src/A7.scala
+++ fst/src/A7.scala
@@ -12,14 +12,15 @@ object MyTime {
   trait Time {
     def ns: Long
     def +(t: Time): Time
+    def *(l:Long): Time
     def isNaTime: Boolean
   }
   // Konversionen auf Long. public
   private lazy val K = 1000L
   implicit class FancyNum(l: Long) {
-    lazy val sec: Time = (l * K).msec
-    lazy val msec: Time = (l * K).μsec
-    lazy val μsec: Time = (l * K).nsec
+    lazy val sec: Time = l.msec*K
+    lazy val msec: Time = l.μsec*K
+    lazy val μsec: Time = l.nsec*K
     lazy val nsec: Time = Time(l)
   }
 
@@ -28,6 +29,7 @@ object MyTime {
     override lazy val ns = undefined
     override def +(t: Time): Time = NaTime
     override lazy val isNaTime = true
+    override def *(l:Long) = NaTime
 
     override def toString = "NaTime"
     override def equals(x: Any): Boolean = false
@@ -39,36 +41,39 @@ object MyTime {
 
     override def +(t: Time): Time = if (t.isNaTime) NaTime else Time(ns + t.ns)
 
+    override def *(l:Long):Time = Time(ns*l)
+
     override def isNaTime = false
 
-    override def equals(o: Any) = {lazy val t = o.asInstanceOf[Time]; o.isInstanceOf[Time] && ns == t.ns}
+    override def equals(o: Any) = {lazy val t = o.asInstanceOf[Time]; o.isInstanceOf[Time] && !t.isNaTime && ns == t.ns}
 
     override def toString = {
-      lazy val sec_cut:Time  = (ns/K/K/K).sec
-      lazy val msec_cut:Time = (ns/K/K).msec
-      lazy val μsec_cut:Time = (ns/K).μsec
-      ns match {
-       // case _ if sec_cut == this => sec_cut + " sec"
-        //case _ if msec_cut == this => msec_cut + " msec"
-        //case _ if μsec_cut == this => μsec_cut + " μsec"
-        case _ => ns + " nsec"
+      val sec_cut:Long  = ns/K/K/K  // get rid of trailing numbers
+      val msec_cut:Long = ns/K/K
+      val μsec_cut:Long = ns/K
+      () match {  // if the lost digits were zeros, then this equality holds.
+        case _ if sec_cut*K*K*K  == ns  => sec_cut  + " sec"
+        case _ if msec_cut*K*K   == ns  => msec_cut + " msec"
+        case _ if μsec_cut*K     == ns  => μsec_cut + " μsec"
+        case _                          => ns       + " nsec"
       }
     }
   }
+  // re-export for visibility
+  def Σ(ts:Time*):Time = Time.Σ(ts)
 
   object Time {
-    // nur positive nanosekunden und 0 sind erlaubt
-    private[MyTime] def apply(l: Long): Time = if (l >= 0) new TimeNS(l) else NaTime
-    def Σ(ts: Time*): Time = ts.foldLeft(Time(0))(_ + _)
+    // Only positive and zero Nanoseconds are valid.
+    private[MyTime] def apply(ns: Long): Time = if (ns >= 0) new TimeNS(ns) else NaTime
+    def Σ(ts: Seq[Time]): Time = ts.foldLeft(Time(0))(_ + _)
   }
 }
+
+
 
 object Ex04 {
   def test {
     import MyTime._
-    import MyTime.Time._
-
-
 
     println("10 sec: " + 10.sec)
     println("20 msec: " + 20.msec)
