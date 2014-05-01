8c19f2b530d974d20b19cd417e2b130743c8b5ed
GollyTicker <golly.ticker@gmail.com>
2014-05-01 12:22:40 +0200

    basic structure correct

--- fst/src/A7.scala
+++ fst/src/A7.scala
@@ -1,35 +1,44 @@
 /**
  * Created by Swaneet on 30.04.2014.
  */
+
 import U.undefined
-object U {def undefined = throw new RuntimeException}
+
+object U {
+  def undefined = throw new RuntimeException
+}
 
 // insteadt of simply saving the number as nsec, μsec, sec etc, I'm saving
 // the decimal digit
-case class Time private (val l: => Long, val isNaTime: => Boolean) {
-  lazy val sec:Time = undefined
-  lazy val msec:Time = undefined
-  lazy val nsec:Time = undefined
-  lazy val μsec:Time = undefined
+case class Time private(val ns: => Long, val isNaTime: => Boolean) {
 
-  def +(t:Time):Time = new Time(l + t.l, isNaTime || t.isNaTime)
+  def +(t: Time): Time = new Time(ns + t.ns, isNaTime || t.isNaTime)
 
-  override def equals(o:Any) = { lazy val t = o.asInstanceOf[Time]; o.isInstanceOf[Time] && nsec == t.nsec}
-  // override
+  override def equals(o: Any) = {
+    lazy val t = o.asInstanceOf[Time]; o.isInstanceOf[Time] && ns == t.ns
+  }
+
+  // override def toString() = undefined
 }
 
 object Time {
-  lazy val NaTime:Time = Time(undefined, true)
-  def Σ(ts:Time*):Time = ts.sum
+  lazy val NaTime: Time = Time(undefined, true)
+
+  def Σ(ts: Time*): Time = ts.sum
+
+  implicit class FancyNum(l: Long) {
+    lazy val sec: Time = undefined
+    lazy val msec: Time = undefined
+    lazy val nsec: Time = undefined
+    lazy val μsec: Time = undefined
+  }
+
 }
 
-object TimeFactory {
-  import math.log10
-  def apply(l:Long):Time = new Time(l, false)
-  def apply(i:Int):Time = TimeFactory(i.toLong)
-}
+object TimeFactory
 
 object Ex04 {
+
   import TimeImplicits._
 
   def test {
@@ -41,14 +50,14 @@ object Ex04 {
     println(1.nsec + 50.nsec + 20.μsec + 3.msec + 4.sec)
     println(3.msec + 4.sec + 1.nsec + 20.μsec + 50.nsec)
     println(Σ(3.msec, 4.sec, 1.nsec, 20.μsec, 50.nsec))
-    10.sec.sec //<- compiliert nicht
-    10.sec+10 //<- compiliert nicht
+    // 10.sec.sec //<- compiliert nicht
+    // 10.sec + 10 //<- compiliert nicht
     println((-1).sec)
     println(Σ(3.msec, 4.sec, 1.nsec, NaTime, 20.μsec, 50.nsec))
   }
 }
 
 object TimeImplicits {
-  implicit def long2Time(l: Long):Time = TimeFactory(l)
-  implicit def int2Time(i: Int):Time = TimeFactory(i)
+  implicit def long2Time(l: Long): Time = undefined
+  implicit def int2Time(i: Int): Time = undefined
 }
