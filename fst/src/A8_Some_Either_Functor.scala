4f4c746b2357f161e7df64b6d4fe4021c04b0d1e
GollyTicker <golly.ticker@gmail.com>
2014-05-09 01:10:03 +0200

    A8 fin!

--- fst/src/A8_Some_Either_Functor.scala
+++ fst/src/A8_Some_Either_Functor.scala
@@ -14,7 +14,10 @@ object A8 {
   }
 
   def testSeq() {
+    println( sequence(List(Some(1), Some(2), None)) )
     println(None == sequence(List(Some(1), Some(2), None)))
+
+    println( sequence(List(Some("Hallo"), Some("Welt"))) )
     println(Some(List("Hallo", "Welt")) == sequence(List(Some("Hallo"), Some("Welt"))))
   }
 
@@ -30,19 +33,22 @@ object A8 {
   val customers = List(Left(Person("P1")), Left(Person("P2")), Right(Company(Person("CEO1"))), Left(Person("P3")),
     Right(Company(Person("CEO2"))), Right(Company(Person("CEO3"))), Left(Person("P3")))
 
-  def sortEither(ls: List[Either[Person, Company]]): List[Either[Person, Company]] = ls match {
-    case Nil => Nil
-    case pivot :: xs => {
-      val smaller = xs.filter(e => cmpEither(e, pivot) <= 0)
-      val bigger = xs.filter(e => cmpEither(e, pivot) > 0)
-      smaller ++ List(pivot) ++ bigger
-    }
-  }
+  import Function.uncurried
 
-  def cmpEither(fst: Either[Person, Company], snd: Either[Person, Company]): Int = () match {
-    //case _ if fst.isLeft && snd.isLeft => fst compare snd // compiliert nicht...... diverging implitic expansion for type ....
-    //case _ if fst.isRight && snd.isRight => fst compare snd
-    case _ => -1
+  def sortEither(ls: List[Either[Person, Company]]): List[Either[Person, Company]] = ls.sortWith( (x,y) => cmpEither(x)(y) < 0 )
+
+  val cmpEither: Either[Person, Company] => Either[Person, Company] => Int =
+    fst => snd => (fst, snd) match {
+        case (Left(x), Left(y)) => x compare y
+        case (Right(x), Right(y)) => x compare y
+        case (Left(_), _) => -1
+        case _ => 1
+    }
+
+  val sortAndSplit: List[Either[Person, Company]] => (List[Either[Person, Company]],List[Either[Person, Company]]) =
+    ls => {
+    val sorted = sortEither(ls)
+    (selectPersons(sorted).map( Left(_) ),selectCompanies(sorted).map( Right(_) ))
   }
 
   val sortResult = (List(Left(Person("P1")), Left(Person("P2")), Left(Person("P3")), Left(Person("P3"))),
@@ -66,70 +72,76 @@ object A8 {
 
     println(customers)
 
-    println(sortEither(customers) == sortResult)
-    println(sortEither(customers))
+    println(sortAndSplit(customers))
+    println(sortAndSplit(customers) == sortResult)
 
-    println(selectPersons(customers) == persons)
     println(selectPersons(customers))
+    println(selectPersons(customers) == persons)
 
-    println(selectCompanies(customers) == companies)
     println(selectCompanies(customers))
+    println(selectCompanies(customers) == companies)
 
 
   }
 
   trait ~>[A, B] extends (A => B) {
-    def applyOrDefault(x:A, default:B):B
+    def applyOrDefault(x: A, default: B): B
   }
 
-  def use1[A, B](f: A => B)(x: A):B = f(x)
+  def use1[A, B](f: A => B)(x: A): B = f(x)
 
-  def use2[A, B](f: A ~> B)(x: A)(default: B):B =
+  def use2[A, B](f: A ~> B)(x: A)(default: B): B =
     f.applyOrDefault(x, default)
 
   val f: Double ~> Double = new (Double => Double) with (Double ~> Double) {
-    def applyOrDefault(x:Double, default:Double):Double = if (x >= 0) this(x) else default
-    def apply(x:Double):Double = math.sqrt(x)
+    def applyOrDefault(x: Double, default: Double): Double = if (x >= 0) this(x) else default
+
+    def apply(x: Double): Double = math.sqrt(x)
   }
 
   def testFunction() = {
-    println( use1(f) (2) )
-    println( use1(f) (2) == math.sqrt(2) )
+    println(use1(f)(2))
+    println(use1(f)(2) == math.sqrt(2))
 
-    println( use1(f) (-2.0) )
-    println( use1(f) (-2.0) != Double.NaN ) // NaN != NaN => true
+    println(use1(f)(-2.0))
+    println(use1(f)(-2.0) != Double.NaN) // NaN != NaN => true
 
-    println( use2(f) (0.0) (-2.0) )
-    println( use2(f) (0.0) (-2.0) == 0.0 )
+    println(use2(f)(0.0)(-2.0))
+    println(use2(f)(0.0)(-2.0) == 0.0)
   }
 
 
   trait Functor[F[_]] {
-    def fmap[A,B](fa:F[A], f: A => B):F[B]
+    def fmap[A, B](fa: F[A], f: A => B): F[B]
   }
 
-  val listFunctor:Functor[List] = new Functor[List] {
-    def fmap[A,B](ls:List[A], f: A => B) = ls.map(f)
+  val listFunctor: Functor[List] = new Functor[List] {
+    def fmap[A, B](ls: List[A], f: A => B) = ls.map(f)
   }
-  val optionFunctor:Functor[Option] = new Functor[Option] {
-    def fmap[A,B](ma:Option[A], f: A => B) = ma.map(f)
+  val optionFunctor: Functor[Option] = new Functor[Option] {
+    def fmap[A, B](ma: Option[A], f: A => B) = ma.map(f)
   }
 
   def testFunctor() = {
 
-    println(listFunctor.fmap(List("Hello", "Students", "!"), (s:String) => s.size ))
-    println(listFunctor.fmap(List("Hello", "Students", "!"), (s:String) => s.size ) == List(5, 8, 1))
+    println(listFunctor.fmap(List("Hello", "Students", "!"), (s: String) => s.size))
+    println(listFunctor.fmap(List("Hello", "Students", "!"), (s: String) => s.size) == List(5, 8, 1))
 
     import scala.collection._
-    val romanNums = Map("I"->1, "II"->2, "III"->3 )  // Idea shows misleading "cannot resolve symbol" bug
+    val romanNums = Map("I" -> 1, "II" -> 2, "III" -> 3) // Idea shows misleading "cannot resolve symbol" bug
 
-    println(optionFunctor.fmap(romanNums.get("II"), (i:Int) => i*2) )
-    println(optionFunctor.fmap(romanNums.get("II"), (i:Int) => i*2) == Some(4))
-    println(optionFunctor.fmap(romanNums.get("IIII"), (i:Int) => i*2) )
-    println(optionFunctor.fmap(romanNums.get("IIII"), (i:Int) => i*2) == None)
+    println(optionFunctor.fmap(romanNums.get("II"), (i: Int) => i * 2))
+    println(optionFunctor.fmap(romanNums.get("II"), (i: Int) => i * 2) == Some(4))
+    println(optionFunctor.fmap(romanNums.get("IIII"), (i: Int) => i * 2))
+    println(optionFunctor.fmap(romanNums.get("IIII"), (i: Int) => i * 2) == None)
   }
 
-
+  def testAll() = {
+    testSeq()
+    testEither()
+    testFunction()
+    testFunctor()
+  }
 
 
 }
