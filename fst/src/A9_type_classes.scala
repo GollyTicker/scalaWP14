11fd07523c3229129c5092accaad5b6a0303fc1c
GollyTicker <golly.ticker@gmail.com>
2014-05-16 14:26:45 +0200

    a1 working

--- fst/src/A9_type_classes.scala
+++ fst/src/A9_type_classes.scala
@@ -8,32 +8,37 @@ object A9 {
   }
 
   def a1() = {
-    val x = new Car(1997,"Ford","E350","ac, abs, moon",3000.00)
+    import CSV._
+    val x = new Car(1997, "Ford", "E350", "ac, abs, moon", 3000.00)
 
     println(x)
     println(csv(x))
 
   }
-
 }
 
-trait CSV[T] {
-  def csv(value :T):String
-}
+object CSV {
 
-object csv {  // shortcut
-  def apply[A:CSV](x: A):String = x.csv(x)
+  trait CSV[T] {
+    def csv(value: T): String
+  }
+
+  object csv {
+    // shortcut
+    def apply[A <: CSV[A]](x: A): String = x.csv(x)
+  }
+
 }
 
 class Car(
-          val year: Int,
-          val make: String,
-          val model: String,
-          val description: String,
-          val price: Double
-         )
-  extends CSV[Car]
-{
-  override def toString():String = List("" + year, make, model, description, "" + price).mkString(",")
+           val year: Int,
+           val make: String,
+           val model: String,
+           val description: String,
+           val price: Double
+           )
+  extends CSV.CSV[Car] {
+  override def toString(): String = List("" + year, make, model, description, "" + price).mkString(",")
+
   def csv(x: Car): String = x.toString
 }
