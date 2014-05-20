1bba9a75cedfb3f78062cca9f3da2f0d5e6456a4
GollyTicker <golly.ticker@gmail.com>
2014-05-20 15:09:45 +0200

    doing A9 again

--- fst/src/A9_type_classes.scala
+++ fst/src/A9_type_classes.scala
@@ -13,14 +13,14 @@ object A9 {
     import CarCSV._
     val x = new Car(1997, "Ford", "E350", "ac, abs, moon", 3000.00)
     println(x)
-    println(useCsv(x))
-
+    println(csv(x))
   }
 }
 
 trait CSV[T] {
-  def csv(value: T): String
+  def toCSV(value: T): String
 }
+
 class Car(
            val year: Int,
            val make: String,
@@ -32,11 +32,12 @@ class Car(
 }
 
 object CarCSV {
-  // useCsv ist die Methode die vom Andender verwendet wird. Sie besagt, dass ich den Kontext von CSV meine.
-  def useCsv[T: CSV](c: T) = implicitly[CSV[T]].csv(c)
-
   // hier der typeclass instance implementation (außerhalb der KLassendefinition)
   implicit val carCSV = new CSV[Car] {
-    def csv(c: Car) = c.toString()
+    def toCSV(c: Car) = c.toString()
   }
+
+  // useCsv ist die Methode die vom Andender verwendet wird. Sie besagt, dass ich den Kontext von CSV meine.
+  def csv[T: CSV](c: T) = implicitly[CSV[T]].toCSV(c)
+
 }
