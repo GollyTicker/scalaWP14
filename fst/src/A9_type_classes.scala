a28692cb90f0565de8aa7265d5809f2dee563f5b
GollyTicker <golly.ticker@gmail.com>
2014-05-20 15:07:09 +0200

    now I know how to use type classes!

--- fst/src/A9_type_classes.scala
+++ fst/src/A9_type_classes.scala
@@ -6,65 +6,21 @@ object A9 {
   def run() = {
     a1()
 
-    A9_A3.a3()
+    // a2()
   }
 
   def a1() = {
     import CarCSV._
     val x = new Car(1997, "Ford", "E350", "ac, abs, moon", 3000.00)
     println(x)
-    println(csv(x))
+    println(useCsv(x))
 
   }
-
-  def a2() = {
-    import MatrixImplicits._
-    val mat = Matrix(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    // println(mat * 5 == 5 * mat) // ???
-  }
-
-
 }
 
 trait CSV[T] {
   def csv(value: T): String
 }
-
-trait Scale[T] {
-  def *(s: Double, x: T): T
-}
-
-object MatrixImplicits {
-
-  implicit object MatrixScale extends Scale[Matrix] {
-    def *(s: Double, x: Matrix): Matrix = x * s
-  }
-
-  implicit object MatrixNumeric extends math.Numeric[Matrix] {
-
-    def plus(x: Matrix, y: Matrix): Matrix = x + y
-
-    def toDouble(x: Matrix): Double = x(0, 0)
-
-    def toFloat(x: Matrix): Float = toDouble(x).toFloat
-
-    def toLong(x: Matrix): Long = toDouble(x).toLong
-
-    def toInt(x: Matrix): Int = toDouble(x).toInt
-
-    def fromInt(x: Int): Matrix = Matrix(1, x)
-
-    def negate(x: Matrix): Matrix = -x
-
-    def times(x: Matrix, y: Matrix): Matrix = x * y
-
-    def minus(x: Matrix, y: Matrix): Matrix = x - y
-
-    def compare(x: Matrix, y: Matrix): Int = ???
-  }
-
-}
-
 class Car(
            val year: Int,
            val make: String,
@@ -75,60 +31,12 @@ class Car(
   override def toString(): String = List("" + year, make, model, description, "" + price).mkString(",")
 }
 
-object CarCSV extends CSV[Car] {
-  // hier der typeclass instance (außerhalb der KLassendefinition)
-  def csv(c: Car): String = c.toString()
-}
+object CarCSV {
+  // useCsv ist die Methode die vom Andender verwendet wird. Sie besagt, dass ich den Kontext von CSV meine.
+  def useCsv[T: CSV](c: T) = implicitly[CSV[T]].csv(c)
 
-trait BaseUnit
-
-trait Meter extends BaseUnit
-
-trait Kelvin extends BaseUnit
-
-trait Second extends BaseUnit
-
-trait Quantity {
-  type unit <: BaseUnit
-  def value: Double
-}
-
-trait QMonoid[Q <: Quantity] {
-  def plus(x: Q, y: Q): Q
-}
-
-case class Length(val d:Double)
-case class Temperature(val d:Double)
-case class Time(val d:Double)
-
-
-object unitImplicits {
-  implicit class Length2(val d:Double) extends Quantity {
-    type unit = Meter
-    val value = d
-  }
-  implicit class Temperature2(val d:Double) extends Quantity {
-    type unit = Kelvin
-    val value = d
-  }
-  implicit class Time2(val d:Double) extends Quantity {
-    type unit = Second
-    val value = d
-  }
-
-  def add[Q <: Quantity : QMonoid](x: Q, y: Q): Q = implicitly[QMonoid[Q]].plus(x, y)
-}
-
-
-
-object A9_A3 {
-  def a3() = {
-    import unitImplicits._
-    //println(add(Length(1), Length(2.7)))
-    //println(add(Temperature(273.15), Temperature(30.0)))
-    //println(add(Time(100), Time(50)))
-    //println(Length(1) + Length(2.7))
-    //println(Temperature(273.15) + Temperature(30.0))
-    //println(Time(100) + Time(50))
+  // hier der typeclass instance implementation (außerhalb der KLassendefinition)
+  implicit val carCSV = new CSV[Car] {
+    def csv(c: Car) = c.toString()
   }
 }
--- fst/src/v10.scala
+++ fst/src/v10.scala
@@ -13,4 +13,78 @@ object v10 {
 
   // Futures verwenden Monad-for um die Funktionen zu komponieren.
   // Ganz am Ende holt man das Ergebnis raus
+
+  // Parallel Collecitons
+  // collection.parallel enthält Traits
+  // collection.parallel.immutable und _.mutable enthält Implementierungen
+  // (Gutes Beispiel von Guten Hierarchien. Nur die Blätter der Hieerarchien sind Klassen und Implementierungen,
+  //  Alles darüber sind nur traits und abstrakt.)
+
+  // ParVector/ ParSet
+
+  // (0 to 100).par.foldLeft(0)(_+_)
+
+  lazy val s = (0 to 10000).par.foldLeft(0)(_+_)
+
+  def mutableSum() = {
+    val arr = (0 to 100).toArray.par
+    var sum=0
+    arr.foreach(sum+=_) // operation ist nicht atomar. f x s = set (get s + x)
+    sum // das Ergebnis ist unterschiedlich.... data races
+  }
+
+  def seqSum() = {
+    val arr = (0 to 100).toArray
+    var sum=0
+    arr.foreach(sum+=_) // operation ist nicht atomar. f x s = set (get s + x)
+    sum // das Ergebnis ist unterschiedlich.... data races
+  }
+
+  def prints() = {
+    for(i <- (0 to 10).par) print(i + " ")
+    println()
+    for(i <- (0 to 10).par) print(i + " ")
+    println()
+    for(i <- (0 to 10).par) print(i + " ")
+    println()
+    for(i <- (0 to 10).par) print(i + " ")
+  }
+
+  def nonassocReduce() = {
+    val arr=(0 to 1000).toArray.par
+    println(arr.reduce(_ - _))
+    println(arr.reduce(_ - _))
+    println(arr.reduce(_ - _))
+    println(arr.reduce(_ - _))
+  }
+
+  def nonassocFold() = {
+    val arr=(0 to 1000).toArray.par
+    println(arr.foldLeft(0)(_ - _))
+    println(arr.foldLeft(0)(_ - _))
+    println(arr.foldLeft(0)(_ - _))
+    println(arr.foldLeft(0)(_ - _))
+  }
+
+  // wichtig bei parallelen Operationen
+  // Assoziativ-Gesetz bei reduce muss gelten.
+  // pure Funktionen (Seiteneffektfreie Programmierung)
+
+  def measure[A](x: => A):Unit = {
+    import System._
+    val t1 = nanoTime()
+    x
+    val t2 = nanoTime() - t1
+    println("<" + (t2/(BigInt(10)^6).toLong) + "ms >")
+  }
+
+  def parPrint2 = {
+    val parArr = (0 to 10000000).par
+    measure(for (i <- parArr) print(if(i%1000000==0) i + " " else ""))
+  }
+
+  def seqPrint2 = {
+    val arr = (0 to 10000000)
+    measure(for (i <- arr) print(if(i%1000000==0) i + " " else ""))
+  }
 }
