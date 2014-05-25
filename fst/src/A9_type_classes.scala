24a18a8d4311d33450ad70a0c8cd04becaefc9f1
GollyTicker <golly.ticker@gmail.com>
2014-05-25 21:38:11 +0200

    type classes finished!!

--- fst/src/A9_type_classes.scala
+++ fst/src/A9_type_classes.scala
@@ -1,3 +1,5 @@
+import com.sun.javafx.tk.quantum.QuantumClipboard
+
 /**
  * Created by Swaneet on 16.05.2014.
  */
@@ -6,7 +8,7 @@ object A9 {
   def run() = {
     a1()
 
-    // a2()
+    baseUnitAufgabe.a3()
   }
 
   def a1() = {
@@ -32,12 +34,90 @@ class Car(
 }
 
 object CarCSV {
-  // hier der typeclass instance implementation (außerhalb der KLassendefinition)
+  // hier der typeclass instance implementation (außerhalb der Klassendefinition)
   implicit val carCSV = new CSV[Car] {
     def toCSV(c: Car) = c.toString()
   }
 
   // useCsv ist die Methode die vom Andender verwendet wird. Sie besagt, dass ich den Kontext von CSV meine.
   def csv[T: CSV](c: T) = implicitly[CSV[T]].toCSV(c)
+}
 
+
+// Aufgabe 3
+
+trait BaseUnit
+
+trait Meter extends BaseUnit
+trait Kelvin extends BaseUnit
+trait Second extends BaseUnit
+
+trait Quantity {
+  type unit <: BaseUnit
+  def value: Double
+}
+
+trait QMonoid[Q <: Quantity] {
+  def plus(x: Q, y: Q): Q
+}
+
+// ================== NEU HINZUGEFÜGT ==========================
+object Implicits {
+
+  // die Methode die der User aufruft
+  def add[Q <: Quantity : QMonoid](x: Q, y: Q): Q = implicitly[QMonoid[Q]].plus(x, y)
+
+  // allgemeine Implementation fur Quantities. Ein Konstruktor muss mit übergeben werden.
+  def QMonoidOver[Q <: Quantity](create:Double => Q): QMonoid[Q] = new QMonoid[Q] {
+    def plus(x: Q, y: Q): Q = create(x.value + y.value)
+  }
+  // erlaubt plus(a,b) auch als a + b zu schreiben
+  implicit class plusImplicits[Q<:Quantity:QMonoid](x:Q) {
+    def +(y:Q) = implicitly[QMonoid[Q]].plus(x,y)
+  }
+
+  // jetzt kann man den Quantities implementationen hinzufügen
+  implicit def LengthMonoid:QMonoid[Length] = QMonoidOver(Length)
+
+  implicit def TemperatureMonoid:QMonoid[Temperature] = QMonoidOver(Temperature)
+
+  implicit def TimeMonoid:QMonoid[Time] = QMonoidOver(Time)
+
+  /*
+  // manuelle TypeClass implementation
+  implicit def LengthMonoid: QMonoid[Length] = new QMonoid[Length] {
+    def plus(x: Length, y: Length): Length = Length(x.value + y.value)
+  }
+  */
+}
+
+// Die Quantity Klassen
+case class Length(x: Double) extends AnyRef with Quantity { // wie ist die Syntax, wenn man nur ein trait implementieren will?
+  type unit = Meter
+  def value: Double = x
+}
+
+case class Temperature(x: Double) extends AnyRef with Quantity { type unit = Kelvin; def value: Double = x }
+
+case class Time(x: Double) extends AnyRef with Quantity { type unit = Second; def value: Double = x }
+
+// ============ ENDE - NEU HINZUGEFÜGT - ENDE ===================
+
+object baseUnitAufgabe {
+  def a3() {
+    import Implicits._
+
+    println(add(Length(1), Length(2.7)))
+    println(add(Temperature(273.15), Temperature(30.0)))
+    println(add(Time(100), Time(50)))
+
+    println((Length(1)+Length(2.7)))
+    println(Temperature(273.15)+Temperature(30.0))
+    println(Time(100)+Time(50))
+
+    // add(Length(1),Time(2))  // <- compiliert nicht
+    // Length(1) + Time(2)  // <- compiliert nicht
+
+    // this is a "shapeless non-compilaton" example !!
+  }
 }
