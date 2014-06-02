639e3be11ed562c2d05a024c578089b625b2fc14
GollyTicker <golly.ticker@gmail.com>
2014-06-02 22:39:38 +0200

    added esser systempros

--- fst/src/myUtils.scala
+++ fst/src/myUtils.scala
@@ -9,4 +9,38 @@ object myUtils {
     println(s"stop $name.")
     r
   }
+
+  object OSEnvironment {
+    import System.{getProperty => prop}
+
+    val numCores= Runtime.getRuntime.availableProcessors
+    val name= prop("os.name","unknown OS")
+    val version= prop("os.version","unknown version")
+    val architecture= prop("os.arch","unknown architecture")
+    val jvm= prop("java.version", "unknown")
+
+    def printOSSpecs {
+      println("Cores : %d".format(numCores))
+      println("OS : %s %s (%s)".format(name,version,architecture))
+      println("JVM : " + jvm)
+    }
+  }
+
+  object ExecTiming {
+    def resultAndNanos[A](block: => A) = {
+      val t = System.nanoTime
+      (block,System.nanoTime - t)
+    }
+
+    def printMillis[A](block: => A) {
+      val (result,nanoTime)= resultAndNanos(block)
+      println("time: "+ "~%.3fms".format((nanoTime/1000L)/1000.0)+ "\n")
+    }
+
+    def printResultAndMillis[A](block: => A) {
+      val (result,nanoTime)= resultAndNanos(block)
+      println(result + "\ntime: "+ "~%.3fms".format((nanoTime/1000L)/1000.0)+ "\n")
+    }
+  }
+
 }
