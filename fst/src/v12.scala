906abc5b556bff6561e4d04ecb0054b0683c6ccb
GollyTicker <golly.ticker@gmail.com>
2014-06-03 15:24:23 +0200

    vorls 12

---
+++ fst/src/v12.scala
@@ -0,0 +1,22 @@
+/**
+ * Created by Swaneet on 03.06.2014.
+ */
+object v12 {
+
+  // Vortrag Shapeless
+  // Vortrag Finagle
+  // Vortrag OpenCL
+
+  def apply() = {
+
+  }
+
+
+}
+
+
+// type MyComplex = Float + Float (( hier 8 Byte primitive zusammenbauen
+
+// case class Complex(r:MyComplex) extends AnyVal
+
+// println(new Complex(1.0F))
